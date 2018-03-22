53499167953d4f12a4d9876c9f181e91f2f9ccc6
Chris Kew <ckew@oculus.co.uk>
2018-03-22 16:22:02 +0000

    Update to gulpfile.js and added Gulp folder with individual task files

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,18 @@
+var gulp = require("gulp"),
+	postcss = require("gulp-postcss"),
+	autoprefixer = require("autoprefixer"),
+	cssvars = require("postcss-simple-vars"),
+	cssImport = require("postcss-import"),
+	mixins = require("postcss-mixins"),
+	nested = require("postcss-nested"),
+	mqpacker = require("css-mqpacker");
+
+gulp.task("styles", function () {
+	return gulp.src("./styles/main.css")
+		.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer, mqpacker]))
+		.on("error", function(errorInfo) {
+			console.log(errorInfo.toString());
+			this.emit("end");
+		})
+		.pipe(gulp.dest("./app/temp/styles"));
+});
