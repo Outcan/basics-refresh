c8b86288640dd251564ee5a298ccfe7a6dbb2877
Chris Kew <chris.kew@gmail.com>
2018-03-23 22:03:46 +0000

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,2 @@
-var gulp = require("gulp"),
-    watch = require("gulp-watch");
-
-
-gulp.task("default", function() {
-    console.log("Default gulp task");
-});
-
-gulp.task("html", function() {
-    console.log("Image something happening to your HTML file");
-});
-
-gulp.task("styles", function() {
-    console.log("Imagine some processing happening to your css files");
-});
-
-gulp.task("watch", function() {
-    watch("./app/index.html", function() {
-        gulp.start("html");
-    });
-    watch("./app/assets/styles/**/*.css", function() {
-        gulp.start("styles");
-    });
-});
+require("./gulp/tasks/styles");
+require("./gulp/tasks/watch");
